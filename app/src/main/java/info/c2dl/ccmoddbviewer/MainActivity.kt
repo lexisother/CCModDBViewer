2b26c46189f3ed936b4f3222c9a962300af8cce9
Alyxia Sother <lexisoth2005@gmail.com>
2021-08-25 13:03:14 +0200

    [Main] Added Xinto's ModListItem component

--- app/src/main/java/info/c2dl/ccmoddbviewer/MainActivity.kt
+++ app/src/main/java/info/c2dl/ccmoddbviewer/MainActivity.kt
@@ -3,9 +3,19 @@ package info.c2dl.ccmoddbviewer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 
 import info.c2dl.ccmoddbviewer.ui.theme.CCModDBViewerTheme
 
@@ -19,9 +29,43 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Text("Hi")
+                    ModListItem(modName = "Test", modDescription = "Test2") {
+                        Text("Hi")
+                    }
                 }
             }
+        }
+    }
+
+    /**
+     * @author X1nto
+     */
+    @OptIn(ExperimentalComposeUiApi::class)
+    @Composable
+    fun ModListItem(
+        modName: String,
+        modDescription: String,
+        content: @Composable () ->  Unit
+    ) {
+        var isDialogVisible by remember { mutableStateOf(false) }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { isDialogVisible = true }
+                .background(Color(525252))
+        ) {
+            Column {
+                Text(text = modName)
+                Text(text = modDescription)
+            }
+        }
+
+        if (isDialogVisible) {
+            Dialog(
+                onDismissRequest = { isDialogVisible = false },
+                properties = DialogProperties(usePlatformDefaultWidth = false),
+                content = content
+            )
         }
     }
 }
