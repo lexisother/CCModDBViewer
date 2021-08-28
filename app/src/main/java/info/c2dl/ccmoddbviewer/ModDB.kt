623719c8d8cd073bf6564395251284372c9921d3
Fuzuki <hades785@gmail.com>
2021-08-28 12:28:45 +0200

    [Main] Migrate from mods.json to npDatabase.json

--- app/src/main/java/info/c2dl/ccmoddbviewer/ModDB.kt
+++ app/src/main/java/info/c2dl/ccmoddbviewer/ModDB.kt
@@ -1,20 +1,16 @@
 package info.c2dl.ccmoddbviewer
 
-data class ModDB(
-    var mods: Map<String, Mod>? = null,
-)
+typealias ModDB = Map<String, Mod>
+
 data class Mod(
-    var name: String? = null,
-    var description: String? = null,
-    var page: List<Page>,
-    var archive_link: String? = null,
-    var hash: Hash? = null,
-    var version: String? = null,
+    val metadata: ModMeta
 )
-data class Page(
-    var name: String? = null,
-    var url: String? = null,
-)
-data class Hash(
-    var sha256: String? = null,
+
+data class ModMeta(
+    val name: String,
+    val version: String,
+    val description: String? = null,
+    val homepage: String? = null,
+    val license: String? = null,
+    val ccmodDependencies: Map<String, String>? = null,
 )
